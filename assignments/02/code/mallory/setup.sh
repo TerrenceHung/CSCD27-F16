a4aa6a9e72b646979d150808ca9d8ed52736e047
ThierrySans <thierry.sans@gmail.com>
2016-11-01 20:13:30 -0400

    A2: add iptables to mallory

--- assignments/02/code/mallory/setup.sh
+++ assignments/02/code/mallory/setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 
-sudo DEBIAN_FRONTEND=noninteractive  apt-get install python python-twisted-web dsniff wireshark -y
+sudo DEBIAN_FRONTEND=noninteractive  apt-get install python python-twisted-web dsniff wireshark iptables -y
 if ! [ -d /home/vagrant/sslstrip-0.9 ]; then
     wget https://moxie.org/software/sslstrip/sslstrip-0.9.tar.gz
     tar xvzf sslstrip-0.9.tar.gz
